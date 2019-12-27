11950ffecc6a932dcbb3976d4ee34aa790d73c1b
saifeddin <saifeddinmatoui4@gmail.com>
2019-12-27 23:48:12 +0100

    initial commit

---
+++ adoption/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.http import HttpResponse, Http404
+
+from .models import Pet 
+
+
+def home(request):
+	pets = Pet.objects.all()
+
+	return render(request, 'home.html', {'pets':pets})
+	#return HttpResponse('<p>Home view</p>')
+
+def pet_detail(request, id):
+	try:
+		pet = Pet.objects.get(id=id)
+	except Pet.DoesNotExist:
+		raise Http404('Pet doesnt exist!!!!')
+
+	return render(request, 'pet_detail.html', {'pet':pet})
+
+	#return HttpResponse('<p>pet detail view with the id {}</p>'.format(id))
+
